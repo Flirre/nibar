68ac2240218d86469114d9696dd4c42b3966078e
Filip Lindahl <filip.g.lindahl@gmail.com>
2020-03-08 22:10:04 +0100

    Updated battery widget

--- lib/Battery.jsx
+++ lib/Battery.jsx
@@ -1,9 +1,22 @@
-import styles from "./styles.jsx";
+import styles from './styles.jsx';
 
 const render = ({ output }) => {
   let charging = output.charging;
   let percentage = output.percentage;
   let remaining = output.remaining;
+  let battery = '';
+  if (percentage < 76) {
+    battery = '';
+  }
+  if (percentage < 51) {
+    battery = '';
+  }
+  if (percentage < 26) {
+    battery = '';
+  }
+  if (percentage < 10) {
+    battery = '';
+  }
   return (
     <div>
       <div
@@ -13,7 +26,12 @@ const render = ({ output }) => {
             : null
         }
       >
-        <span>{charging ? "􀋦" : null} {percentage}%</span>
+        <span>
+          <span style={{ fontFamily: 'FontAwesome' }}>
+            {charging ? '􀋦' : battery}
+          </span>{' '}
+          {percentage}% ({remaining})
+        </span>
       </div>
     </div>
   );
